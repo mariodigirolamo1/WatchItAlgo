9ab7d2b4052b2a44146eba8e7f74fffdc2c7ddc7
mariodigirolamo1 <mariodigirolamowork@gmail.com>
2023-11-18 11:22:54 +0100

    feat(Autoplay): add autoplay with different speed
    Concurrency is sloppy, should be more organized.

--- app/src/main/java/com/mdg/watchitalgo/screens/bubblesort/BubbleSort.kt
+++ app/src/main/java/com/mdg/watchitalgo/screens/bubblesort/BubbleSort.kt
@@ -1,8 +1,10 @@
 package com.mdg.watchitalgo.screens.bubblesort
 
+import android.widget.Space
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -19,6 +21,7 @@ import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,6 +34,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.mdg.watchitalgo.R
 import com.mdg.watchitalgo.common.model.Algorithm
 import com.mdg.watchitalgo.common.ui.theme.Typography
@@ -43,6 +47,7 @@ fun BubbleSort(
     val array = bubbleSortViewModel.array.collectAsState()
     val currentIndex = bubbleSortViewModel.currentIndex.collectAsState()
     val sorted = bubbleSortViewModel.sorted.collectAsState()
+    val autoplaySpeed = bubbleSortViewModel.autoplaySpeed.collectAsState()
 
     Surface(
         modifier = Modifier
@@ -60,8 +65,14 @@ fun BubbleSort(
             ){ currentIndex.value }
             Spacer(modifier = Modifier.size(80.dp))
             AlgorithmControls(
-                resetAlgorithmState = { bubbleSortViewModel.resetAlgorithmState() }
-            ) { bubbleSortViewModel.bubbleSortStep() }
+                toggleAutoplay = { bubbleSortViewModel.toggleAutoPlay() },
+                resetAlgorithmState = { bubbleSortViewModel.resetAlgorithmState() },
+                getAutoplaySpeed = { autoplaySpeed.value },
+                updateAutoplaySpeed = { newSpeed: Float ->
+                    bubbleSortViewModel.updateAutoplaySpeed(newSpeed = newSpeed)
+                },
+                restartAutoplay = { bubbleSortViewModel.restartAutoplayWithNewSpeed() },
+            ) { bubbleSortViewModel.bubbleSortStep(manualStart = true) }
         }
     }
 }
@@ -112,26 +123,51 @@ private fun Bars(
 
 @Composable
 private fun AlgorithmControls(
+    toggleAutoplay: () -> Unit,
     resetAlgorithmState: () -> Unit,
+    getAutoplaySpeed: () -> Float,
+    updateAutoplaySpeed: (newSpeed: Float) -> Unit,
+    restartAutoplay: () -> Unit,
     bubbleSortStep: () -> Unit
 ){
-    Row(
-        horizontalArrangement = Arrangement
-            .spacedBy(
-                space = 8.dp,
-                alignment = Alignment.CenterHorizontally
-            ),
-        modifier = Modifier
-            .fillMaxWidth()
+    Column(
+        modifier = Modifier.fillMaxWidth()
     ) {
-        Button(onClick = resetAlgorithmState) {
-            Text(text = stringResource(R.string.reset_button_text))
+        Row(
+            horizontalArrangement = Arrangement
+                .spacedBy(
+                    space = 8.dp,
+                    alignment = Alignment.CenterHorizontally
+                ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Button(onClick = resetAlgorithmState) {
+                Text(text = stringResource(R.string.reset_button_text))
+            }
+            Button(onClick = bubbleSortStep) {
+                Text(text = stringResource(R.string.next_button_text))
+            }
+            Button(onClick = toggleAutoplay) {
+                Text(text = stringResource(R.string.autoplay_button_text))
+            }
         }
-        Button(onClick = bubbleSortStep) {
-            Text(text = stringResource(R.string.next_button_text))
+        Spacer(modifier = Modifier.size(size = 30.dp))
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Text(text = "Autoplay speed")
         }
-        Button(onClick = {}) {
-            Text(text = stringResource(R.string.autoplay_button_text))
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Slider(
+                value = getAutoplaySpeed(),
+                onValueChange = updateAutoplaySpeed,
+                onValueChangeFinished = restartAutoplay,
+                valueRange = 0.1f .. 2f
+            )
         }
     }
 }
--- app/src/main/java/com/mdg/watchitalgo/screens/bubblesort/BubbleSortViewModel.kt
+++ app/src/main/java/com/mdg/watchitalgo/screens/bubblesort/BubbleSortViewModel.kt
@@ -2,76 +2,154 @@ package com.mdg.watchitalgo.screens.bubblesort
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
+import java.util.Timer
+import kotlin.concurrent.timer
 
 class BubbleSortViewModel: ViewModel() {
     private var _array = MutableStateFlow(unorderedStartingArray)
-    val array = _array
+    val array: StateFlow<IntArray> = _array
 
     private var _currentIndex = MutableStateFlow(0)
-    val currentIndex = _currentIndex
+    val currentIndex: StateFlow<Int> = _currentIndex
 
     private var _sorted = MutableStateFlow(false)
     val sorted = _sorted
 
+    private var _autoplaySpeed = MutableStateFlow(1f)
+    val autoplaySpeed: StateFlow<Float> = _autoplaySpeed
+
+    private val bubbleSortStepMutex = Mutex()
     private var animateSwap = false
     private var swapped = true
 
-    fun resetAlgorithmState(){
-        viewModelScope.launch {
-            _array.emit(unorderedStartingArray)
-            _currentIndex.emit(0)
-            _sorted.emit(false)
-            withContext(Dispatchers.Default){
-                animateSwap = false
-                swapped = true
+    private val autoplayMutex = Mutex()
+    private var isAutoPlaying = false
+    private var autoplayTimer: Timer? = null
+
+    fun updateAutoplaySpeed(newSpeed: Float){
+        viewModelScope.launch(Dispatchers.Default) {
+            autoplayMutex.withLock {
+                withContext(Dispatchers.Main.immediate){
+                    _autoplaySpeed.emit(newSpeed)
+                }
             }
         }
     }
 
-    fun bubbleSortStep(){
+    fun restartAutoplayWithNewSpeed(){
         viewModelScope.launch(Dispatchers.Default) {
-            val tmpArray = IntArray(_array.value.size){0}
-            _array.value.forEachIndexed {index, value ->
-                tmpArray[index] = value
+            autoplayMutex.withLock {
+                stopAutoplay()
             }
-            val tmpCurrentIndex = currentIndex.value
+            toggleAutoPlay()
+        }
+    }
 
-            if(!animateSwap){
-                if(tmpCurrentIndex == 0 && swapped){
-                    swapped = false
-                }
-                if(
-                    tmpCurrentIndex < tmpArray.size - 1
-                ){
-                    if(tmpArray[tmpCurrentIndex] > tmpArray[tmpCurrentIndex+1]){
-                        doSwap(tmpArray = tmpArray, tmpCurrentIndex = tmpCurrentIndex)
-                    }else{
-                        withContext(Dispatchers.Main.immediate){
-                            _currentIndex.emit(_currentIndex.value+1)
-                        }
-                    }
-                }else{
-                    if(!swapped){
-                        withContext(Dispatchers.Main.immediate){
-                            _sorted.emit(true)
-                        }
-                    }else{
-                        withContext(Dispatchers.Main.immediate){
-                            _currentIndex.emit(0)
-                        }
-                    }
-                }
+    fun resetAlgorithmState(){
+        viewModelScope.launch(Dispatchers.Default) {
+            bubbleSortStepMutex.withLock {
                 withContext(Dispatchers.Main.immediate){
-                    _array.emit(tmpArray)
+                    _array.emit(unorderedStartingArray)
+                    _currentIndex.emit(0)
+                    _sorted.emit(false)
                 }
-            }else{
-                triggerColorAnimation()
+                animateSwap = false
+                swapped = true
+            }
+            autoplayMutex.withLock {
+                stopAutoplay()
             }
         }
+    }
+
+    fun toggleAutoPlay() = runBlocking{
+        withContext(Dispatchers.Default){
+            autoplayMutex.withLock {
+                val period = (1000L / autoplaySpeed.value).toLong()
+                println()
+                if(isAutoPlaying){
+                    isAutoPlaying = false
+                    autoplayTimer?.cancel()
+                }else{
+                    isAutoPlaying = true
+                    autoplayTimer = timer(
+                        name = "autoplay timer",
+                        initialDelay = 0,
+                        daemon = false,
+                        period = period
+                    ){
+                        if(!_sorted.value){
+                            bubbleSortStep(manualStart = false)
+                        }else{
+                            stopAutoplay()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun bubbleSortStep(manualStart: Boolean) = runBlocking{
+        viewModelScope.launch(Dispatchers.Default) {
+            if(manualStart){
+                autoplayMutex.withLock {
+                    stopAutoplay()
+                }
+            }
+            bubbleSortStepMutex.withLock {
+                val tmpArray = IntArray(_array.value.size){0}
+                _array.value.forEachIndexed {index, value ->
+                    tmpArray[index] = value
+                }
+                val tmpCurrentIndex = currentIndex.value
+
+                if(!animateSwap){
+                    if(tmpCurrentIndex == 0 && swapped){
+                        swapped = false
+                    }
+                    if(
+                        tmpCurrentIndex < tmpArray.size - 1
+                    ){
+                        if(tmpArray[tmpCurrentIndex] > tmpArray[tmpCurrentIndex+1]){
+                            doSwap(tmpArray = tmpArray, tmpCurrentIndex = tmpCurrentIndex)
+                        }else{
+                            withContext(Dispatchers.Main.immediate){
+                                _currentIndex.emit(_currentIndex.value+1)
+                            }
+                        }
+                    }else{
+                        if(!swapped){
+                            withContext(Dispatchers.Main.immediate){
+                                _sorted.emit(true)
+                            }
+                        }else{
+                            withContext(Dispatchers.Main.immediate){
+                                _currentIndex.emit(0)
+                            }
+                        }
+                    }
+                    withContext(Dispatchers.Main.immediate){
+                        _array.emit(tmpArray)
+                    }
+                }else{
+                    triggerColorAnimation()
+                }
+            }
+        }
+    }
+
+    private fun stopAutoplay(){
+        autoplayTimer?.cancel()
+        isAutoPlaying = false
     }
 
     private fun doSwap(
