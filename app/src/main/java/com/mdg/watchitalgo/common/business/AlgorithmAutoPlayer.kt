6e4cfec86a019ca54541783fc34fb5ef20d4e089
mariodigirolamo1 <mariodigirolamowork@gmail.com>
2023-11-29 21:24:00 +0100

    refactor(AutoPlayer): extract autoplayer logic from bubble sort view model

---
+++ app/src/main/java/com/mdg/watchitalgo/common/business/AlgorithmAutoPlayer.kt
@@ -0,0 +1,97 @@
+package com.mdg.watchitalgo.common.business
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
+import java.util.Timer
+import kotlin.concurrent.timer
+
+// TODO: fix bug, since now if i change speed autoplay stops 
+class AlgorithmAutoPlayer {
+    private val autoplayMutex = Mutex()
+    private var autoplayTimer: Timer? = null
+
+    var isAutoPlaying = false
+        get() = runBlocking {
+            // TODO: inject dispatcher for testing
+            withContext(Dispatchers.Default){
+                autoplayMutex.withLock {
+                    field
+                }
+            }
+        }
+        set(value) = runBlocking {
+            // TODO: inject dispatcher for testing
+            withContext(Dispatchers.Default){
+                autoplayMutex.withLock {
+                    field = value
+                }
+            }
+        }
+    var speed = 1.0f
+        get() = runBlocking {
+            // TODO: inject dispatcher for testing
+            withContext(Dispatchers.Default){
+                autoplayMutex.withLock {
+                    field
+                }
+            }
+        }
+        set(value) = runBlocking {
+            // TODO: inject dispatcher for testing
+            withContext(Dispatchers.Default){
+                autoplayMutex.withLock {
+                    field = value
+                }
+            }
+        }
+
+    suspend fun stop() {
+        withContext(Dispatchers.Default) {
+            autoplayTimer?.cancel()
+            isAutoPlaying = false
+        }
+    }
+
+    suspend fun restartAutoplayWithNewSpeed(
+        // todo: find a better name
+        action: () -> Unit
+    ) {
+        withContext(Dispatchers.Default){
+            autoplayTimer?.cancel()
+            if(isAutoPlaying){
+                val period = (1000L / speed).toLong()
+                autoplayTimer = timer(
+                    // TODO: remove those magic values
+                    name = "autoplay timer",
+                    initialDelay = 0,
+                    daemon = false,
+                    period = period
+                ){ action() }
+            }else{
+                isAutoPlaying = false
+            }
+        }
+    }
+
+    // TODO: refactor accordingly to restartAutoplayWithNewSpeed
+    suspend fun toggleAutoPlay(
+        action: () -> Unit
+    ) {
+        withContext(Dispatchers.Default){
+            val period = (1000L / speed).toLong()
+            if(isAutoPlaying){
+                autoplayTimer?.cancel()
+            }else{
+                autoplayTimer = timer(
+                    name = "autoplay timer",
+                    initialDelay = 0,
+                    daemon = false,
+                    period = period
+                ){ action() }
+            }
+        }
+    }
+}
--- app/src/main/java/com/mdg/watchitalgo/screens/bubblesort/BubbleSortViewModel.kt
+++ app/src/main/java/com/mdg/watchitalgo/screens/bubblesort/BubbleSortViewModel.kt
@@ -2,18 +2,19 @@ package com.mdg.watchitalgo.screens.bubblesort
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
+import com.mdg.watchitalgo.common.business.AlgorithmAutoPlayer
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
-import java.util.Timer
-import kotlin.concurrent.timer
 
+// TODO: isolate bubble sort steps
+//  state of the algorithm stays here to have a coherent display in the ui but
+//  logic for the algorithm needs to be placed elsewhere, this viewModel does too
+//  many things
 class BubbleSortViewModel: ViewModel() {
     private var _array = MutableStateFlow(unorderedStartingArray)
     val array: StateFlow<IntArray> = _array
@@ -21,49 +22,41 @@ class BubbleSortViewModel: ViewModel() {
     private var _currentIndex = MutableStateFlow(0)
     val currentIndex: StateFlow<Int> = _currentIndex
 
-    private var _sorted = MutableStateFlow(false)
-    val sorted = _sorted
-
     private var _autoplaySpeed = MutableStateFlow(1f)
     val autoplaySpeed: StateFlow<Float> = _autoplaySpeed
+
+    private var _isAutoPlaying = MutableStateFlow(false)
+    val isAutoPlaying: StateFlow<Boolean> = _isAutoPlaying
+
+    private var _sorted = MutableStateFlow(false)
+    val sorted = _sorted
 
     private val bubbleSortStepMutex = Mutex()
     private var animateSwap = false
     private var swapped = true
 
-    // TODO: all this autoplay logic does not belong here
-    //  will be held in new package/module for shared timer logic
-    private val autoplayMutex = Mutex()
-    private var _isAutoPlaying = MutableStateFlow(false)
-    val isAutoPlaying: StateFlow<Boolean> = _isAutoPlaying
-    private var autoplayTimer: Timer? = null
+    private val algorithmAutoPlayer = AlgorithmAutoPlayer()
+
+    init{
+        viewModelScope.launch {
+            with(algorithmAutoPlayer) {
+                _autoplaySpeed.emit(speed)
+                _isAutoPlaying.emit(isAutoPlaying)
+            }
+        }
+    }
 
     fun updateAutoplaySpeed(newSpeed: Float){
-        viewModelScope.launch(Dispatchers.Default) {
-            autoplayMutex.withLock {
-                withContext(Dispatchers.Main.immediate){
-                    _autoplaySpeed.emit(newSpeed)
-                }
-            }
+        viewModelScope.launch {
+            algorithmAutoPlayer.speed = newSpeed
+            _autoplaySpeed.emit(algorithmAutoPlayer.speed)
         }
     }
 
     fun restartAutoplayWithNewSpeed(){
         viewModelScope.launch(Dispatchers.Default) {
-            autoplayMutex.withLock {
-                autoplayTimer?.cancel()
-                if(_isAutoPlaying.value){
-                    val period = (1000L / autoplaySpeed.value).toLong()
-                    autoplayTimer = timer(
-                        name = "autoplay timer",
-                        initialDelay = 0,
-                        daemon = false,
-                        period = period
-                    ){ timerTask() }
-                }else{
-                    _isAutoPlaying.emit(false)
-                }
-            }
+            algorithmAutoPlayer.restartAutoplayWithNewSpeed { timerTask() }
+            _isAutoPlaying.emit(algorithmAutoPlayer.isAutoPlaying)
         }
     }
 
@@ -78,27 +71,15 @@ class BubbleSortViewModel: ViewModel() {
                 animateSwap = false
                 swapped = true
             }
-            stopAutoplay()
+            algorithmAutoPlayer.stop()
+            _isAutoPlaying.emit(algorithmAutoPlayer.isAutoPlaying)
         }
     }
 
-    fun toggleAutoPlay() = runBlocking{
-        withContext(Dispatchers.Default){
-            autoplayMutex.withLock {
-                val period = (1000L / autoplaySpeed.value).toLong()
-                if(_isAutoPlaying.value){
-                    _isAutoPlaying.emit(false)
-                    autoplayTimer?.cancel()
-                }else{
-                    _isAutoPlaying.emit(true)
-                    autoplayTimer = timer(
-                        name = "autoplay timer",
-                        initialDelay = 0,
-                        daemon = false,
-                        period = period
-                    ){ timerTask() }
-                }
-            }
+    fun toggleAutoPlay(){
+        viewModelScope.launch {
+            algorithmAutoPlayer.toggleAutoPlay { timerTask() }
+            _isAutoPlaying.emit(algorithmAutoPlayer.isAutoPlaying)
         }
     }
 
@@ -107,14 +88,18 @@ class BubbleSortViewModel: ViewModel() {
             if(!_sorted.value){
                 bubbleSortStep(manualStart = false)
             }else{
-                stopAutoplay()
+                algorithmAutoPlayer.stop()
+                _isAutoPlaying.emit(algorithmAutoPlayer.isAutoPlaying)
             }
         }
     }
 
-    fun bubbleSortStep(manualStart: Boolean) = runBlocking{
+    fun bubbleSortStep(manualStart: Boolean) {
         viewModelScope.launch(Dispatchers.Default) {
-            if(manualStart){ stopAutoplay() }
+            if(manualStart){
+                algorithmAutoPlayer.stop()
+                _isAutoPlaying.emit(algorithmAutoPlayer.isAutoPlaying)
+            }
             bubbleSortStepMutex.withLock {
                 val tmpArray = IntArray(_array.value.size){0}
                 _array.value.forEachIndexed {index, value ->
@@ -154,13 +139,6 @@ class BubbleSortViewModel: ViewModel() {
                     triggerColorAnimation()
                 }
             }
-        }
-    }
-
-    private suspend fun stopAutoplay(){
-        autoplayMutex.withLock {
-            autoplayTimer?.cancel()
-            _isAutoPlaying.emit(false)
         }
     }
 
